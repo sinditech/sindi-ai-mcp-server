37f99babd1c38399aa4fc53d823cfa85e134c943
Buhake Sindi <buhake_sindi@hotmail.com>
2025-05-10 02:06:43 +0200

    Bug fix.

--- sindi-ai-mcp-server-spi/src/main/java/za/co/sindi/ai/mcp/server/runtime/impl/JSONDataTypes.java
+++ sindi-ai-mcp-server-spi/src/main/java/za/co/sindi/ai/mcp/server/runtime/impl/JSONDataTypes.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import za.co.sindi.commons.utils.Primitives;
 
@@ -31,6 +32,7 @@ public class JSONDataTypes {
 		types.put(Map.class, "object");
 		types.put(Date.class, "string");
 		types.put(Temporal.class, "string");
+		types.put(UUID.class, "string");
 		types.put(Object.class, "object");
 		JAVA_DATA_TYPES = Collections.unmodifiableMap(types);
 	}
@@ -40,6 +42,8 @@ public class JSONDataTypes {
 	}
 	
 	public static String deterimineJsonType(final Class<?> clazz) {
+		if (clazz == null) return "null";
+		
 		final Class<?> type = clazz.isArray() ? Collection.class : clazz.isPrimitive() ? Primitives.wrap(clazz) : clazz ;
 		Optional<String> jsonType = JAVA_DATA_TYPES.entrySet().stream().filter(entry -> entry.getKey().isAssignableFrom(type)).map(Map.Entry::getValue).findFirst();
 		return jsonType.orElseThrow(() -> new IllegalArgumentException("Could not map type '" + clazz + "' to a respective JSON type."));
